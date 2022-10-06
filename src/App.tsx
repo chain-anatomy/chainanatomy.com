d60379a93e5d7389fda4f09f251ca6d996d4073b
reaperes <reaperes105@gmail.com>
2022-10-06 14:30:29 +0900

    First release

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from './company-banner.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
