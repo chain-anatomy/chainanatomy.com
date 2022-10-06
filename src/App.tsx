944282d1e9697cd6cbf73a384a160a48cf243695
reaperes <reaperes105@gmail.com>
2022-10-06 16:23:52 +0900

    Update home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import logo from './company-banner.png';
+import underConstruction from './under-construction.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
+      <div className="loadingContainer">
+        <div className="spinner"/>
+      </div>
+      <img src={underConstruction} className="underConstruction" alt="updating" />
     </div>
   );
 }
