12532b07be24c2226a84af4ff6a71154f691cd44
reaperes <reaperes105@gmail.com>
2022-10-06 17:33:07 +0900

    Update logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import logo from './company-banner.png';
 import underConstruction from './under-construction.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
+      <video autoPlay muted width="82%">
+        <source type="video/webm" src="/logo.webm"/>
+      </video>
       <div className="loadingContainer">
         <div className="spinner"/>
       </div>
