278216af23c28a2dbe746496329303bd7c5d2cb7
reaperes <reaperes105@gmail.com>
2022-11-07 17:29:10 +0900

    [feature] add slide5 enhanced scroll

--- src/pages/slides/Slide5.tsx
+++ src/pages/slides/Slide5.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import classNames from "classnames"
 import { slideClass, slideStyle } from "./SlideConfig"
 import earthFrame from './earth-frame.png'
@@ -14,20 +15,24 @@ type Slide5ContainerProps = {
 }
 
 function Slide5Container(props: Slide5ContainerProps) {
+  function ServiceImage() {
+    return (
+      <div className="flex items-center">
+        <img className="" style={{ height: '460px' }} src={props.imageSrc} alt="" />
+      </div>
+    )
+  }
+
   return (
-    <div id="services" className="snap-center shrink-0 flex flex-col w-full">
+    <div id="services" className="relative snap-center shrink-0 flex flex-col w-full overflow-hidden px-40">
       <h1 className="text-center text-main-green">{props.title}</h1>
 
-      <div className="relative flex-1 flex flex-row mt-12 pl-20">
-        { props.imageDirection === 'left' && (
-          <div className="flex-1 flex justify-center justify-items-center px-40">
-            <img className="object-contain" src={props.imageSrc} alt="" />
-          </div>
-        )}
+      <div className="relative flex flex-row justify-evenly mt-12 mb-32">
+        { props.imageDirection === 'left' && <ServiceImage />}
 
-        <div className="flex-1">
-          { props.descriptions.map(description => (
-            <div className="flex flex-col mb-8">
+        <div className="flex flex-col justify-center">
+          { props.descriptions.map((description, idx) => (
+            <div className={classNames("flex flex-col", { "mb-8": idx !== props.descriptions.length - 1 })}>
               <h3>{description.listLabel}</h3>
               <ul className="marker:text-main-gray list-disc pl-6">
                 {description.listItems.map(item => (
@@ -38,128 +43,146 @@ function Slide5Container(props: Slide5ContainerProps) {
           ))}
         </div>
 
-        { props.imageDirection === 'right' && (
-          <div className="flex-1 flex justify-center justify-items-center px-40">
-            <img className="object-contain" src={props.imageSrc} alt="" />
-          </div>
-        )}
+        { props.imageDirection === 'right' && <ServiceImage />}
       </div>
+
     </div>
   )
 }
 
 export default function Slide5() {
+  const [slidePos, setSlidePos] = useState(0)  // 0, 1, 2, 3
+
+  const handlerScroll = e => {
+    const slidesTotalWidth = e.target.offsetWidth
+    const scrollPosX = e.target.scrollLeft
+    const calibratePosX = slidesTotalWidth / 4
+
+    setSlidePos(Math.floor((scrollPosX + calibratePosX) / slidesTotalWidth))
+  }
+
   return (
-    <div className={classNames(slideClass, 'flex flex-row my-24 snap-x snap-mandatory overflow-y-none overflow-x-scroll')}
-         style={{ ...slideStyle, justifyContent: 'start' }}>
+    <div className={classNames(slideClass, 'relative flex flex-row my-24')} style={slideStyle}>
 
-      <Slide5Container
-        title="Strategy / Operation"
-        imageDirection="right"
-        imageSrc={earthFrame}
-        descriptions={[{
-          listLabel: "Project System Architecture",
-          listItems: [
-            "Blockchain Service flow",
-            "Data source managing plan",
-            "Blockchain project tokenomics",
-            "Server Database",
-          ],
-        }, {
-          listLabel: 'On/Off-chain interaction plan',
-          listItems: [
-            'Blockchain component customizing',
-            'NFT-fi / Staking',
-            'Lending / NFT Dao',
-          ],
-        }, {
-          listLabel: 'NFT functions',
-          listItems: [
-            'Merge',
-            'Split',
-            'Breeding',
-          ],
-        }]}
-      />
+      <div className="flex flex-row snap-x snap-mandatory overflow-y-none overflow-x-scroll" onScroll={handlerScroll}>
+        <Slide5Container
+          title="Strategy / Operation"
+          imageDirection="right"
+          imageSrc={earthFrame}
+          descriptions={[{
+            listLabel: "Project System Architecture",
+            listItems: [
+              "Blockchain Service flow",
+              "Data source managing plan",
+              "Blockchain project tokenomics",
+              "Server Database",
+            ],
+          }, {
+            listLabel: 'On/Off-chain interaction plan',
+            listItems: [
+              'Blockchain component customizing',
+              'NFT-fi / Staking',
+              'Lending / NFT Dao',
+            ],
+          }, {
+            listLabel: 'NFT functions',
+            listItems: [
+              'Merge',
+              'Split',
+              'Breeding',
+            ],
+          }]}
+        />
 
-      <Slide5Container
-        title="Infrastructure"
-        imageDirection="left"
-        imageSrc={infrastructure}
-        descriptions={[{
-          listLabel: 'Node Operating',
-          listItems: ['ethereum, klaytn, matic, solana, bsc, etc'],
-        }, {
-          listLabel: 'IPFS Operating',
-          listItems: [
-            'ipfs private gateway',
-            'ipfs-metadata management',
-          ],
-        }, {
-          listLabel: 'Public Cloud Management (AWS)',
-          listItems: [
-            'Domain Management',
-            'Backend Server Management',
-            'S3 Data Management',
-          ],
-        }]}
-      />
+        <Slide5Container
+          title="Infrastructure"
+          imageDirection="left"
+          imageSrc={infrastructure}
+          descriptions={[{
+            listLabel: 'Node Operating',
+            listItems: ['ethereum, klaytn, matic, solana, bsc, etc'],
+          }, {
+            listLabel: 'IPFS Operating',
+            listItems: [
+              'ipfs private gateway',
+              'ipfs-metadata management',
+            ],
+          }, {
+            listLabel: 'Public Cloud Management (AWS)',
+            listItems: [
+              'Domain Management',
+              'Backend Server Management',
+              'S3 Data Management',
+            ],
+          }]}
+        />
 
-      <Slide5Container
-        title="Web 3.0"
-        imageDirection="right"
-        imageSrc={web3}
-        descriptions={[{
-          listLabel: 'Custom Smart Contract',
-          listItems: [
-            '[Publishing] ERC20, ERC721, ERC1155, ERC5114 / etc',
-            '[Publishing] vesting, locking, upgradable (proxy), DAO',
-            '[Publishing] NFT merge / split',
-            '[DeFi] swap, staking, lending',
-          ],
-        }, {
-          listLabel: 'Service',
-          listItems: [
-            'On/off chain bridging api',
-            'on-chain data sync',
-            'on-chain event publishing',
-            'on-chain data indexing / searching',
-          ],
-        }, {
-          listLabel: 'Marketplace',
-          listItems: [
-            'Simple marketplace (smart contract only)',
-            'Gas optimized marketplace (wyvern/seaport protocol)',
-          ],
-        }]}
-      />
+        <Slide5Container
+          title="Web 3.0"
+          imageDirection="right"
+          imageSrc={web3}
+          descriptions={[{
+            listLabel: 'Custom Smart Contract',
+            listItems: [
+              '[Publishing] ERC20, ERC721, ERC1155, ERC5114 / etc',
+              '[Publishing] vesting, locking, upgradable (proxy), DAO',
+              '[Publishing] NFT merge / split',
+              '[DeFi] swap, staking, lending',
+            ],
+          }, {
+            listLabel: 'Service',
+            listItems: [
+              'On/off chain bridging api',
+              'on-chain data sync',
+              'on-chain event publishing',
+              'on-chain data indexing / searching',
+            ],
+          }, {
+            listLabel: 'Marketplace',
+            listItems: [
+              'Simple marketplace (smart contract only)',
+              'Gas optimized marketplace (wyvern/seaport protocol)',
+            ],
+          }]}
+        />
 
-      <Slide5Container
-        title="Web 3.0 LiveOps"
-        imageDirection="right"
-        imageSrc={liveOps}
-        descriptions={[{
-          listLabel: 'Live Token distribution',
-          listItems: [
-            'Visualization where token transfer',
-          ],
-        }, {
-          listLabel: 'Token/Contract flow',
-          listItems: [
-            'Live data for each contract',
-            'Supply monitor - incentive viewer',
-            'Burn monitor - breeding fee, market fee, function fee, etc',
-            'LP size tracker',
-          ],
-        }, {
-          listLabel: 'Target management',
-          listItems: [
-            'Price targeting',
-            'Inflation targeting',
-            'Tx management',
-          ],
-        }]}
-      />
+        <Slide5Container
+          title="Web 3.0 LiveOps"
+          imageDirection="right"
+          imageSrc={liveOps}
+          descriptions={[{
+            listLabel: 'Live Token distribution',
+            listItems: [
+              'Visualization where token transfer',
+            ],
+          }, {
+            listLabel: 'Token/Contract flow',
+            listItems: [
+              'Live data for each contract',
+              'Supply monitor - incentive viewer',
+              'Burn monitor - breeding fee, market fee, function fee, etc',
+              'LP size tracker',
+            ],
+          }, {
+            listLabel: 'Target management',
+            listItems: [
+              'Price targeting',
+              'Inflation targeting',
+              'Tx management',
+            ],
+          }]}
+        />
+      </div>
+
+      <div className="absolute left-1/2 bottom-8 -translate-x-1/2">
+          <div className="flex space-x-4">
+            <div className={classNames("w-4 h-4 rounded-full", { 'bg-main-gray': slidePos !== 0, 'bg-main-green': slidePos === 0})} />
+            <div className={classNames("w-4 h-4 rounded-full", { 'bg-main-gray': slidePos !== 1, 'bg-main-green': slidePos === 1})} />
+            <div className={classNames("w-4 h-4 rounded-full", { 'bg-main-gray': slidePos !== 2, 'bg-main-green': slidePos === 2})} />
+            <div className={classNames("w-4 h-4 rounded-full", { 'bg-main-gray': slidePos !== 3, 'bg-main-green': slidePos === 3})} />
+          </div>
+      </div>
+
     </div>
   )
 }
