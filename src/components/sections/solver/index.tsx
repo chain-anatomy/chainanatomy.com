d5084b134c75e6359256768f760bb9f512671434
YoonsooKim9 <yoonsoo.kim@chainanatomy.com>
2024-08-22 11:26:21 +0900

    feat: update member info

--- src/components/sections/solver/index.tsx
+++ src/components/sections/solver/index.tsx
@@ -1,143 +1,156 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef } from 'react';
 
-import * as S from "./style";
+import * as S from './style';
 
-import BgImg from "assets/images/section6/bg.png";
-import MediaBr from "components/common/mobileBr";
+import BgImg from 'assets/images/section6/bg.png';
+import MediaBr from 'components/common/mobileBr';
 
-import { TitActive, TitBasic } from "components/common/sectionTit/style";
+import { TitActive, TitBasic } from 'components/common/sectionTit/style';
 
 const memberInfo = [
   {
-    position: "CEO",
-    name: "Jongun Choi",
+    position: 'CEO',
+    name: 'Jongun Choi',
     careers: [
-      "Gangwon Science High School",
-      "Korea University. Mechanical Engineering",
-      "Former) ABC QUANT(Shenzen, China) Founding Member",
-      "Former) MMX, Web3.0 Senior director",
-      "Former) MONOSIG Investment Mange dept. Executive director",
+      'Gangwon Science High School',
+      'Korea University. Mechanical Engineering',
+      'Former) ABC QUANT(Shenzen, China) Founding Member',
+      'Former) MMX, Web3.0 Senior director',
+      'Former) MONOSIG Investment Mange dept. Executive director',
     ],
     works: [
-      "On-chain, Off-chain algorithm",
-      "Trading strategy Management",
-      "Web3.0 service planning & operating",
+      'On-chain, Off-chain algorithm',
+      'Trading strategy Management',
+      'Web3.0 service planning & operating',
     ],
   },
   {
-    position: "Strategy Lead",
-    name: "Yousang Jo",
+    position: 'Strategy Lead',
+    name: 'Yousang Jo',
     careers: [
-      "Gangwon Science High School",
-      "KAIST. M.S. Management engineering(Finance)",
-      "Former) Kyobo Life Investment Manage dept",
-      "Former) MONOSIG Quant 1 Team",
+      'Gangwon Science High School',
+      'KAIST. M.S. Management engineering(Finance)',
+      'Former) Kyobo Life Investment Manage dept',
+      'Former) MONOSIG Quant 1 Team',
     ],
     works: [
-      "On-chain, Off-chain algorithm",
-      "Trading strategy Management",
-      "On-chain data analysis products planning",
-      "Web3.0 Tokenomics analytics & valuation",
-      "Financial data analysis",
+      'On-chain, Off-chain algorithm',
+      'Trading strategy Management',
+      'On-chain data analysis products planning',
+      'Web3.0 Tokenomics analytics & valuation',
+      'Financial data analysis',
     ],
   },
   {
-    position: "CTO",
-    name: "Namhoon Kim",
+    position: 'CTO',
+    name: 'Namhoon Kim',
     careers: [
-      "Mechanical Engineering from Konkuk University - NHN NEXT",
-      "Former) Tech Lead at Coupang and Woowa Brothers Corp",
-      "Former) Co-founder and Team Lead Developer at Recover39",
-      "Former) MONOSIG Quant 1 Team",
+      'Mechanical Engineering from Konkuk University - NHN NEXT',
+      'Former) Tech Lead at Coupang and Woowa Brothers Corp',
+      'Former) Co-founder and Team Lead Developer at Recover39',
+      'Former) MONOSIG Quant 1 Team',
     ],
     works: [
-      "Service architect / DevOps",
-      "Fullstack Developer",
-      "Smart contract (Solidity)",
-      "Quant / Defi system",
+      'Service architect / DevOps',
+      'Fullstack Developer',
+      'Smart contract (Solidity)',
+      'Quant / Defi system',
     ],
   },
   {
-    position: "Product Manager",
-    name: "Junyeop Lee",
+    position: 'Product Manager',
+    name: 'Junyeop Lee',
     careers: [
-      "Daeil Foreign Language Highschool",
-      "Coventry University",
-      "Fomer) Knance Strategy & Planning Dept",
-      "Former) MONOSIG Strategy & Planning Dept",
+      'Daeil Foreign Language Highschool',
+      'Coventry University',
+      'Fomer) Knance Strategy & Planning Dept',
+      'Former) MONOSIG Strategy & Planning Dept',
     ],
     works: [
-      "Crypto asset market analysis / Business development",
-      "Global market target Web3.0 products planning",
-      "On-chain data analysis products planning",
-      "Crypto asset custody service planning",
+      'Crypto asset market analysis / Business development',
+      'Global market target Web3.0 products planning',
+      'On-chain data analysis products planning',
+      'Crypto asset custody service planning',
     ],
   },
   {
-    position: "Software Engineer",
-    name: "IkSung Jo",
+    position: 'Software Engineer',
+    name: 'IkSung Jo',
     careers: [
-      "Incheon Science High School",
-      "KAIST. Computer Science",
-      "Former) NHN NEXT. Game Server Programming",
-      "Former) FunAt. Softeware Engineer",
-      "Former) Randebu. Softeware Engineer, co-founder",
-      "Former) Ruby Groupe. System Engineer",
+      'Incheon Science High School',
+      'KAIST. Computer Science',
+      'Former) NHN NEXT. Game Server Programming',
+      'Former) FunAt. Softeware Engineer',
+      'Former) Randebu. Softeware Engineer, co-founder',
+      'Former) Ruby Groupe. System Engineer',
     ],
     works: [
-      "Android application Development (Java)",
-      "Hybrid mobile application Development (Ionic framework/angular JS)",
-      "Mobile application Backend Development (C#)",
-      "e-Commerce Development (Saleforce platform/Rhino JS)",
+      'Android application Development (Java)',
+      'Hybrid mobile application Development (Ionic framework/angular JS)',
+      'Mobile application Backend Development (C#)',
+      'e-Commerce Development (Saleforce platform/Rhino JS)',
     ],
   },
   {
-    position: "Software Engineer",
-    name: "JongSung Hwang",
+    position: 'Software Engineer',
+    name: 'JongSung Hwang',
     careers: [
-      "NHN NEXT",
-      "Former) NHN Entertainment Developer",
-      "Former) SUPERCAT Game Content Dev. Part Leader",
-      "Former) MONOSIG Quant 1 Team",
+      'NHN NEXT',
+      'Former) NHN Entertainment Developer',
+      'Former) SUPERCAT Game Content Dev. Part Leader',
+      'Former) MONOSIG Quant 1 Team',
     ],
     works: [
-      "Quant system Development",
-      "DeFi platform Development (Solidity)",
-      "Web Backend Development (Nodejs/typescript)",
-      "Web Frontend Development (React/typescript)",
+      'Quant system Development',
+      'DeFi platform Development (Solidity)',
+      'Web Backend Development (Nodejs/TypeScript)',
+      'Web Frontend Development (React/TypeScript)',
     ],
   },
   {
-    position: "Software Engineer",
-    name: "InHo Jeong",
+    position: 'Software Engineer',
+    name: 'InHo Jeong',
     careers: [
-      "Computer Science from Ajou University",
-      "NHN Next (NEXT Institute)",
-      "Former) NHN Entertainment Developer",
-      "Former) Neptune) Game Develop Team",
-      "Former) Nimble Neuron) Game Develop Team",
-      "Former) Neowiz) Game Develop Team",
-      "Former) Monosig) Quant 1 Team Web3.0 and Web 2.0 Development",
+      'Computer Science from Ajou University',
+      'NHN Next (NEXT Institute)',
+      'Former) NHN Entertainment Developer',
+      'Former) Neptune. Game Develop Team',
+      'Former) Nimble Neuron. Game Develop Team',
+      'Former) Neowiz. Game Develop Team',
+      'Former) Monosig. Quant 1 Team Web3.0 and Web 2.0 Development',
     ],
     works: [
-      "Smart Contract Contract (Solidity)",
-      "Blockchain Node Operating (ethereum/klaytn/hyperledger)",
-      "NodeJS Backend Framework",
-      "React.js Frontend Framework",
+      'Smart Contract Contract (Solidity)',
+      'Blockchain Node Operating (ethereum/klaytn/hyperledger)',
+      'NodeJS Backend Framework',
+      'React.js Frontend Framework',
     ],
   },
   {
-    position: "Software Engineer",
-    name: "ByeongHun Kim",
+    position: 'Software Engineer',
+    name: 'ByeongHun Kim',
     careers: [
-      "Jeonbuk National University",
-      "Annoymous Team, BNBChain Hackathon Track3 : Lifestyle in Web3 - 3rd Place",
-      "Contract Team, 3rd Term of Korea University Blockchain Society",
+      'Jeonbuk National University',
+      'Annoymous Team, BNBChain Hackathon Track3 : Lifestyle in Web3 - 3rd Place',
+      'Contract Team, 3rd Term of Korea University Blockchain Society',
     ],
     works: [
-      "Web Backend Development (nodejs/typescript)",
-      "Blockchain Node Operating (ethereum/klaytn/hyperledger)",
-      "Smart Contract (solidity)",
+      'Web Backend Development (Nodejs/TypeScript)',
+      'Blockchain Node Operating (ethereum/klaytn/hyperledger)',
+      'Smart Contract (solidity)',
+    ],
+  },
+  {
+    position: 'Software Engineer',
+    name: 'Yoonsoo Kim',
+    careers: [
+      'Korea University. Material Science and Engineering',
+      'Former) Van-F. Software Engineer',
+    ],
+    works: [
+      'Smart Contract Contract (Solidity)',
+      'Web Backend Development (Nodejs/TypeScript)',
+      'Web Frontend Development (React, Next.js/TypeScript)',
     ],
   },
 ];
@@ -146,7 +159,7 @@ const Section6 = forwardRef<HTMLElement>((props, ref) => {
   return (
     <S.Section6 ref={ref}>
       <S.BgImg>
-        <img src={BgImg} alt="Donut structure" />
+        <img src={BgImg} alt='Donut structure' />
       </S.BgImg>
       <S.SectionCtnr>
         <S.CustomSectionTit>
@@ -169,7 +182,7 @@ const Section6 = forwardRef<HTMLElement>((props, ref) => {
           <TitActive>e</TitActive>
           <TitActive>m</TitActive>
           &nbsp;
-          <MediaBr media="mobile" />
+          <MediaBr media='mobile' />
           <TitActive>S</TitActive>
           <TitActive>O</TitActive>
           <TitActive>L</TitActive>
@@ -179,12 +192,14 @@ const Section6 = forwardRef<HTMLElement>((props, ref) => {
           <TitActive>S</TitActive>
         </S.CustomSectionTit>
         <S.Desc>
-          Chain Anatomy brings together Korea's top class blockchain experts and financial experts.
+          Chain Anatomy brings together Korea's top class blockchain experts and
+          financial experts.
           <br />
-          Blockchain specialized programming, dAPP Development, Project consulting and Management.
+          Blockchain specialized programming, dAPP Development, Project
+          consulting and Management.
           <br />
-          <br />
-          A team that can support everything you need to succeed in the Web3.0 market.
+          <br />A team that can support everything you need to succeed in the
+          Web3.0 market.
         </S.Desc>
         <S.Experts>
           {memberInfo.map(({ position, name, careers, works }, index) => (
@@ -201,7 +216,7 @@ const Section6 = forwardRef<HTMLElement>((props, ref) => {
               <S.Works>
                 {works.map((work, index) => (
                   <S.Work key={index}>
-                    {work.split("\n").map((line, index) => (
+                    {work.split('\n').map((line, index) => (
                       <div key={index}>
                         {line}
                         <br />
